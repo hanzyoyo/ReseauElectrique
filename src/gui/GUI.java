77d9d678516ab1d4158cb33e16831e7e656a7ae5
Yoann Estepa <yoann.estepa@gmail.com>
2015-02-13 20:07:24 +0100

    mise en place de la GUI

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,42 +1,71 @@
 package gui;
 
-import jade.util.leap.ArrayList;
+
+import jade.core.Agent;
+import jade.wrapper.StaleProxyException;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
-import client, fournisseurs;
+import client.*;
+import fournisseurs.*;
 
-public class GUI {
-	public static void main(String[] args) {
-		ArrayList<ClientAgent> clients = new ArrayList<ClientAgent>;
-		
-	}
+public class GUI extends Agent{
 	
-	public void instantiateGUI(){
-		JFrame fenetre2 = new JFrame("Premier contenu");
+	public void setup(){
+		JFrame fenetre2 = new JFrame("Réseau Electrique");
 		fenetre2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		fenetre2.setPreferredSize(new Dimension(600, 400));
 
 		fenetre2.setLayout(new GridLayout(2, 2));
 
-		fenetre2.add(new JLabel("Texte1"));
+		fenetre2.add(new JLabel("Producteurs"));
+		
+		//TODO : list producers and make them clickable to show their properties pane
 		fenetre2.add(new JTextArea("Vous pouvez modifier ce texte",4,15));
-		fenetre2.add(new JCheckBox("cochez moi !"));
-		fenetre2.add(new JButton("clic ?"));
-		fenetre2.add(new JLabel("Texte2"));
-		JTextArea t = new JTextArea("Vous ne pouvez pas modifier celui-ci",4,15);
-		t.setEditable(false);
-		fenetre2.add(t);
+		//
+		
+		JButton newProdButton = new JButton("Nouveau Producteur");
+		fenetre2.add(newProdButton);
+		newProdButton.addActionListener(new NewProdListener());
+		
+		JButton newClientButton = new JButton(("Nouveau Client"));
+		fenetre2.add(newClientButton);
+		newClientButton.addActionListener(new NewClientListener());
 
 		fenetre2.pack();
 		fenetre2.setVisible(true);
 	}
 
+	class NewProdListener implements ActionListener{
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Nouveau Producteur");
+		}
+		
+	}
+	
+	//listener who creates a new ClientAgent in the same controller when the button is clicked
+	class NewClientListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				//TODO : how to create UNIQUE agent? no logs?
+				getContainerController().createNewAgent("Bob", "client.ClientAgent", null);
+			} catch (StaleProxyException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+	}
 }
